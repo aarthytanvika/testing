dae6d64ace7f928efaa0ecae83f70660994d40e0
balas <deepalearn@hotmail.com>
2023-02-27 18:04:01 +0530

    testing set

---
+++ case1/src/case11/Hashset.java
@@ -0,0 +1,22 @@
+package case11;
+
+
+
+	import java.util.HashSet;
+
+
+	public class Hashset {
+
+		public static void main(String[] args) {
+			HashSet<String> hs=new HashSet<String>();
+			hs.add("ravi");
+			hs.add("flower");
+			hs.add("ship");
+			hs.add("temple");
+			hs.add("community");
+			System.out.println(" enter all:"+hs);
+			
+
+		}
+
+	}
